d5584e1303e7b57a80e91e1a36bf0bdc0240d1f6
renatodcn <renatodcn@outlook.com>
2022-11-06 23:25:01 -0300

    changes in css

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 function createGame(player1, hour, player2) {
   return `
     <li>
-      <img src="/assets/icons/icon=${player1}.svg" alt="Bandeira do ${player1}" />
+      <img src="./assets/icons/icon=${player1}.svg" alt="Bandeira do ${player1}" />
       <strong>${hour}</strong>
-      <img src="assets/icons/icon=${player2}.svg" alt="Bandeira do ${player2}" />
+      <img src="./assets/icons/icon=${player2}.svg" alt="Bandeira do ${player2}" />
     </li>
   `
 }
